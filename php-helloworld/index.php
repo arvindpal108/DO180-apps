9218106b013ea295af24cd443c8c107f8b133d87
Student User <student@workstation.lab.example.com>
2023-05-05 12:42:49 -0400

    updated app

--- php-helloworld/index.php
+++ php-helloworld/index.php
@@ -1,4 +1,4 @@
 <?php
 print "Hello, World! php version is " . PHP_VERSION . "\n";
-print "A change is a coming! by Arvind Pal\n";
+print "A change is a coming! by Arvind Pal 05-05-2023, all is well \n";
 ?>
